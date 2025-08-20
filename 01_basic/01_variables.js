dbdff5842d8284e5a7f835309d140c135841e325
Paritosh3151 <121626126+Paritosh3151@users.noreply.github.com>
2025-08-20 20:35:38 +0000

    datatype

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=134567
+let accountEmail="paritosh@gmail.com"
+var accountPassword="12345"
+accountCity="delhi"
+//accountId=2
+console.log(accountId);
+
+accountEmail="paritosh01@gmail.com"
+accountPassword="2345"
+accountCity="noida"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
